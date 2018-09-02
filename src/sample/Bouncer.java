e963ad1ac758de60066843df3ab8134e7c7a62fe
vincentliu98 <jie.liu0731@gmail.com>
2018-09-02 11:45:46 -0400

    brick bounce

--- src/sample/Bouncer.java
+++ src/sample/Bouncer.java
@@ -4,35 +4,27 @@ import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.Node;
-import java.util.Random;
 
 public class Bouncer {
-    public static final int BOUNCER_MIN_SPEED = -100;
-    public static final int BOUNCER_MAX_SPEED = 100;
-    public static final int BOUNCER_MIN_SIZE = 1;
-    public static final int BOUNCER_MAX_SIZE = 2;
+    public static final int BOUNCER_SPEED = 100;
+    public static final double BOUNCER_SCALE = 1.5;
 
-    private Random dice = new Random();
     public ImageView myBouncer;
-    private Point2D myVelocity;
+    public Point2D myVelocity;
 
     // constructor
     // x and y are the width and height of the canvas
     public Bouncer (Image image, int x, int y) {
         myBouncer = new ImageView(image);
 
-        // make sure it stays a circle. set x and y the same
-        int size = getRandomInRange(BOUNCER_MIN_SIZE, BOUNCER_MAX_SIZE);
-        myBouncer.setScaleX(size);
-        myBouncer.setScaleY(size);
-
         // make sure it stays within the bounds
-        myBouncer.setX(getRandomInRange(size, x-size));
-        myBouncer.setY(getRandomInRange(size, y-size));
+        myBouncer.setX(x/3);
+        myBouncer.setY(y/2);
+        myBouncer.setScaleX(BOUNCER_SCALE);
+        myBouncer.setScaleY(BOUNCER_SCALE);
 
         // turn speed into velocity that can be updated on bounces
-        myVelocity = new Point2D(getRandomInRange(BOUNCER_MIN_SPEED, BOUNCER_MAX_SPEED),
-                getRandomInRange(BOUNCER_MIN_SPEED, BOUNCER_MAX_SPEED));
+        myVelocity = new Point2D(BOUNCER_SPEED, BOUNCER_SPEED);
     }
 
     // make the ball move
@@ -42,20 +34,15 @@ public class Bouncer {
     }
 
     // make the ball bounce off the screen
-    public void bounce (double screenWidth, double screenHeight) {
+    public void bounceWall (double screenWidth) {
         // collide with horizontal direction
         if (myBouncer.getX() < 0 || myBouncer.getX() > screenWidth - myBouncer.getBoundsInLocal().getWidth()) {
             myVelocity = new Point2D(-myVelocity.getX(), myVelocity.getY());
         }
-        // collide with vertical direction
-        if (myBouncer.getY() < 0 || myBouncer.getY() > screenHeight - myBouncer.getBoundsInLocal().getHeight()) {
+        // collide with upper part
+        if (myBouncer.getY() < 0) {
             myVelocity = new Point2D(myVelocity.getX(), -myVelocity.getY());
         }
-    }
-
-    // Returns a non-zero random value in the range (min, max)
-    private int getRandomInRange (int min, int max) {
-        return min + dice.nextInt(max-min) + 1;
     }
 
     // Returns internal view of bouncer to interact with other JavaFX methods.
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -24,17 +25,20 @@ public class Main extends Application {
     public static final int FRAMES_PER_SECOND = 60;
     public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-    public static final Paint BACKGROUND = Color.AZURE;
+    public static final Paint BACKGROUND = Color.WHITE;
     public static final String BOUNCER_IMAGE = "ball.gif";
     public static final String BRICK_IMAGE = "brick1.gif";
     public static final String PADDLE_IMAGE = "paddle.gif";
-    public static final int MOVER_SPEED = 5;
+    public static final int MOVER_SPEED = 20;
+    public static final int BRICKS_COLUMN = 6;
+    public static int BRICKS_ROW = 5;
+    public ImageView[][] myBrick = new ImageView[BRICKS_ROW][BRICKS_COLUMN];
+
 
     // some things we need to remember during our game
     private Scene myScene;
     private ImageView myPaddle;
     private Bouncer myBouncer;
-    private ImageView myBrick;
 
     /**
      * Initialize what will be displayed and how it will be updated.
@@ -62,21 +66,33 @@ public class Main extends Application {
         // make some shapes and set their properties
         var ball_image = new Image(this.getClass().getClassLoader().getResourceAsStream(BOUNCER_IMAGE));
         var paddle_image = new Image(this.getClass().getClassLoader().getResourceAsStream(PADDLE_IMAGE));
-        var brick_image = new Image(this.getClass().getClassLoader().getResourceAsStream(BRICK_IMAGE));
-        myBouncer = new Bouncer(ball_image, width, height);
-        myPaddle = new ImageView(paddle_image);
-        myBrick = new ImageView(brick_image);
 
+        // create bricks
+        var brick_image = new Image(this.getClass().getClassLoader().getResourceAsStream(BRICK_IMAGE));
+//       70 System.out.println(brick_image.getWidth());
+//       20 System.out.println(brick_image.getHeight());
+
+        for (int i = 0; i < BRICKS_ROW; i++) {
+            for (int j = 0; j < BRICKS_COLUMN; j++) {
+                myBrick[i][j] = new ImageView(brick_image);
+                myBrick[i][j].setX(j*brick_image.getWidth()+50*j+20);
+                myBrick[i][j].setY(i*brick_image.getHeight()+50*i+20);
+                root.getChildren().add(myBrick[i][j]);
+            }
+        }
+
+        // add bouncer and paddle
+        myBouncer = new Bouncer(ball_image, width, height);
+        System.out.println(myBouncer.getView().getBoundsInLocal());
+        myPaddle = new ImageView(paddle_image);
         // position the elements
-        myPaddle.setX(width/2 - myPaddle.getFitWidth()/2);
-        myPaddle.setY(height - myPaddle.getFitHeight() - 20);
-        myBrick.setX(width/2 - myBrick.getFitWidth()/2);
-        myBrick.setY(height/2 - myBrick.getFitHeight()/2);
+        myPaddle.setX(width/2 - myPaddle.getLayoutX());
+        myPaddle.setY(height - myPaddle.getLayoutY() - 10);
+        myPaddle.setScaleX(2);myPaddle.setScaleY(2);
 
         // add ball, brick, and paddle
         root.getChildren().add(myBouncer.getView());
         root.getChildren().add(myPaddle);
-        root.getChildren().add(myBrick);
         scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
         return scene;
     }
@@ -87,7 +103,30 @@ public class Main extends Application {
         // make the bouncers move
         myBouncer.move(elapsedTime);
         // make the bouncers bounce
-        myBouncer.bounce(myScene.getWidth(), myScene.getHeight());
+        myBouncer.bounceWall(myScene.getWidth());
+
+        // deal with bouncing off the paddle
+        if (myBouncer.getView().getBoundsInLocal().intersects(myPaddle.getBoundsInLocal())) {
+            myBouncer.myVelocity = new Point2D(myBouncer.myVelocity.getX(), -myBouncer.myVelocity.getY());
+        }
+
+        // deal with bouncing off the bricks
+        for (int i = 0; i < BRICKS_ROW; i++) {
+            for (int j = 0; j < BRICKS_COLUMN; j++) {
+                if (myBouncer.getView().getBoundsInLocal().intersects(myBrick[i][j].getBoundsInLocal())) {
+                    // if x intersects, change y speed
+                    if (Math.abs(myBouncer.getView().getLayoutX() - myBrick[i][j].getX()) >= 0 ||
+                            myBouncer.getView().getBoundsInLocal().getMinX() <= myBrick[i][j].getBoundsInLocal().getMaxX()) {
+                        myBouncer.myVelocity = new Point2D(myBouncer.myVelocity.getX(), -myBouncer.myVelocity.getY());
+                    }
+                    if (myBouncer.getView().getBoundsInLocal().getMaxY() >= myBrick[i][j].getBoundsInLocal().getMinY() ||
+                            myBouncer.getView().getBoundsInLocal().getMinY() <= myBrick[i][j].getBoundsInLocal().getMaxY()) {
+                        // if y intersects, change x speed
+                        myBouncer.myVelocity = new Point2D(-myBouncer.myVelocity.getX(), myBouncer.myVelocity.getY());
+                    }
+                }
+            }
+        }
     }
 
     // What to do each time a key is pressed
